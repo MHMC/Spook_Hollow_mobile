6b02c82a18b0fea0d35c76622a181099b8a5928e
Chase Park <chasepark@comcast.net>
2015-10-16 19:55:16 -0500

    Initial Commit

---
+++ spook-hollow/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  spook-hollow
+//
+//  Created by Chase Park on 10/16/15.
+//  Copyright © 2015 CCL Studios. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
